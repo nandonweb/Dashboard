56850b00e63b0415277cb73632952e68f8850245
Nando <nandosilva98@outlook.com>
2021-08-07 02:34:30 -0300

    Add files via upload

---
+++ cadastro/script2.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    document.getElementById('sidebar').style.width = '250px';
+    document.getElementById('conteudo').style.marginLeft = '250px';
+}
+
+function fecharMenu() {
+    document.getElementById('sidebar').style.width = '0px';
+    document.getElementById('conteudo').style.marginLeft = '0px';
+}
