ee4269c850208ca2c0ffc22567a8a951ab3e2b1e
Nando <nandosilva98@outlook.com>
2021-08-18 10:41:53 -0300

    Update config.php

--- bd/config.php
+++ bd/config.php
@@ -4,7 +4,7 @@ $servidor = "localhost"; // localhost
 $bdname = "investimentos"; // banco de dados
 $bdusuario = "root"; //usuario do bd
 $bdsenha = ""; //senha do bd
-$conexao = mysqli_connect($investimentos, $bdusuario, $bdsenha, $bdname);
+$conexao = mysqli_connect($servidor, $bdusuario, $bdsenha, $bdname);
 
 if ("!$conexao") {
     die("Sem Conexao com o Banco de Dados" . mysqli_connect_error()); 
